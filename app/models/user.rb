44d39498743ced2a56db43f65fb6572c43528542
James Quick <james.q.quick@outlook.com>
2016-11-06 17:11:00 +0000

    Permitted username parameter during signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :user_books
+  has_many :user_books, dependent: :destroy
   has_many :books, through: :user_books
+  validates :password, :presence => true, length: {in: 6..20}
+  validates :email, :presence => true, :uniqueness => true
+  validates :username, :presence => true, :uniqueness => true, length: { in: 6..20 }
 end
